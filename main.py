438187bfa1b8691ae9ed764073180f1dcffeec90
boorishboy <wojciech.chamski@gmail.com>
2021-03-18 19:24:44 +0100

    innitial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix
+sns.set_style('dark_grid')
+
+
+
